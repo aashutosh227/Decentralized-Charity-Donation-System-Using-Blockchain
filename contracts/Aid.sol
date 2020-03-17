268642e7d9acd87a206574f9fc578fd92d6aeebe
aashutosh227 <aashutoshsingh126@gmail.com>
2020-03-18 02:20:21 +0530

    Aid contract updated

--- contracts/Aid.sol
+++ contracts/Aid.sol
@@ -1,16 +1,21 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+import "./User.sol";
+
 contract Aid{
+    User public user;
     struct quotation{
-        uint id;
+        uint256 id;
         address ngo_address;
         string subject;
         string description;
         bool approved;
+        uint256 fund_reqd;
+        uint256 fund_del;
     }
     uint256 id;
     mapping(address=>quotation) public qList;
-    mapping(uint=>address) public itemAddress;
+    mapping(uint256=>address) public itemAddress;
     address[] public quotation_address;
 
     constructor() public{
@@ -20,20 +25,38 @@ contract Aid{
     event Quotation(
         address indexed _address,
         string _subject,
-        string _description
+        string _description,
+        uint256 id
     );
 
-    function setQuotation(address _address, string memory subject, string memory description) 
-        public payable returns(uint256 qid){
+    function setQuotation(address _address, string memory subject, string memory description, 
+    uint256 fund_reqd) public payable returns(uint256 qid){
+        qid = id;
         id += 1;
+        //Create a variable of type  quotation struct
         quotation memory quotDetail;
+        //Set the data into to object variable
+        quotDetail.id = qid;
         quotDetail.ngo_address = msg.sender;
         quotDetail.subject = subject;
         quotDetail.description = description;
-        quotDetail.approved = true;
+        quotDetail.approved = false;
+        quotDetail.fund_reqd = fund_reqd;
+        quotDetail.fund_del = 0;
+
+        //Push the address of ngo  
         quotation_address.push(_address);
+        itemAddress[qid] = _address;
         qList[_address] = quotDetail;
-        emit Quotation(qList[_address].ngo_address, qList[_address].subject, qList[_address].description);
-        return id;
+        emit Quotation(qList[_address].ngo_address, qList[_address].subject,
+        qList[_address].description, qid);
+        return qid;
     }
+
+    function getItemAddress(uint256 qid) public view returns(address _addr){
+        _addr = quotation_address[qid];
+        return _addr;
+    }
+
+    
 }
